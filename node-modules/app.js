c52581b938e34014dc5ee2a4bbb97ee7099f6ed1
Niranjan Bommidi <niranjanprasad.b@gmail.com>
2017-07-31 07:05:09 +0530

    Modules in Node

---
+++ node-modules/app.js
@@ -0,0 +1,5 @@
+var utility = require('./utility');
+
+console.log(utility.counter(['Harsha', 'Anvi', 'Jaanvi']));
+console.log(utility.adder(5, 10));
+console.log(utility.adder(5,utility.pi));
---
+++ node-modules/utility.js
@@ -0,0 +1,11 @@
+var counter = function(inputArray){
+  return  "Input Array has " +inputArray.length+ " elements";
+};
+
+var adder = function(a,b) {
+  return `Sum of two numbers ${a+b}`;
+};
+
+var pi = 3.14;
+
+module.exports = {counter:counter, adder:adder, pi:pi};
